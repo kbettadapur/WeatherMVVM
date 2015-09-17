b138fe8dd3b6e713394076917946234b7b8f8221
Kirin Bettadapur <kirinbetta97@gmail.com>
2015-09-17 16:55:01 -0400

    Removed bin from gitignore

---
+++ WeatherAppMVVM/Models/HourlyData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WeatherAppMVVM.Models
+{
+    public class HourlyData
+    {
+        [JsonProperty("time")]
+        public long UnixTime { get; set; }
+        public string Time
+        {
+            get { return FromUnixTime(UnixTime).ToString(); }
+        }
+
+        public DateTime FromUnixTime(long unixTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unixTime);
+        }
+    }
+}
